16d2fafde7f4c9fdc6cc31da8ae50cca9f3d6eb6
Hasaruariyaratne <ushanhasaranga2003@gmail.com>
2026-01-13 10:08:47 +0530

    Updates

---
+++ assets/minus-1AzlvUZq.js
@@ -0,0 +1,12 @@
+import { c as createLucideIcon } from "./index-CZQwUtkl.js";
+/**
+ * @license lucide-react v0.555.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */
+const __iconNode = [["path", { d: "M5 12h14", key: "1ays0h" }]];
+const Minus = createLucideIcon("minus", __iconNode);
+export {
+  Minus as M
+};
